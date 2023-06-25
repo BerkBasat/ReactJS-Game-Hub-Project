928ea8019991e95a5927d74040331b54053300f2
Berk Basat <82957009+BerkBasat@users.noreply.github.com>
2023-06-25 11:34:37 +0300

    Update api-client.ts

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -9,7 +9,7 @@ export interface FetchResponse<T> {
 const axiosInstance = axios.create({
     baseURL: "https://api.rawg.io/api",
     params:{
-        key: "d5584497724a48d9a075d2b39c3c6c6d"
+        key: ""
     }
 })
 
